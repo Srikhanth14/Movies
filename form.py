41c9a9d0a9bfe72005605218b5d372c18e6e87c7
SRIKHANTH R <139539680+Srikhanth14@users.noreply.github.com>
2024-01-02 23:27:18 +0530

    Update form.py

--- form.py
+++ form.py
@@ -8,15 +8,15 @@ Created on Tue Jan  2 18:04:44 2024
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
+from joblib import load
 
 def rating_prediction():
     # Load your trained model 
-    loaded_model = pickle.load(open('titanic_trained_model.joblib', 'rb'))
+    loaded_model = load('titanic_trained_model.joblib')
 
     # Creating function for prediction
     def movie_rating_prediction(input_data):
-        input_data_np_array = np.asarray(input_data)
+        input_data_np_array = np.asarray(input_data, dtype=float)
         input_data_reshaped = input_data_np_array.reshape(1, -1)
         column_names = ['Year', 'Duration', 'Votes', 'Weighted_Rating']
 
@@ -24,13 +24,14 @@ def rating_prediction():
         prediction = loaded_model.predict(input_data_reshaped_df)
         st.success(f"The predicted movie rating is: {prediction}")
 
-   
     st.title('Movie Rating Prediction Form')
     st.write("Enter the Movie Details: ")
-    Year = st.text_input("Movie Release Year")
-    Duration = st.text_input("Movie Duration (in minutes)")
-    Votes = st.text_input("Number of Votes")
-    Weighted_Rating = st.text_input("Weighted Rating")
+    
+    # Convert inputs to appropriate numerical types
+    Year = st.text_input("Movie Release Year", type='number')
+    Duration = st.text_input("Movie Duration (in minutes)", type='number')
+    Votes = st.text_input("Number of Votes", type='number')
+    Weighted_Rating = st.text_input("Weighted Rating", type='number')
 
     if st.button('Predict Movie Rating'):
-        movie_rating_prediction([Year,Duration,Votes,Weighted_Rating])
+        movie_rating_prediction([Year, Duration, Votes, Weighted_Rating])
